300d2b72fb01454222501fa08fd3ad81195f88eb
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-15 11:53:45 -0300

    Estrutura switch-case, substitui o if else

---
+++ SwitchCaseAula87/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SwitchCaseAula87
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um numero de 1 a 7 para saber o dia da semana: ");
+            int x = int.Parse(Console.ReadLine());
+            string day;
+            Console.WriteLine();
+
+            // Essa solucao substitui os "if else" encadeados:
+
+            switch (x)
+            {
+                case 1:
+                    day = "Sunday";
+                    break;
+                case 2:
+                    day = "Monday";
+                    break;
+
+                case 3:
+                    day = "Tuesday";
+                    break;
+
+                case 4:
+                    day = "Wednesday";
+                    break;
+
+                case 5:
+                    day = "Thursday";
+                    break;
+
+                case 6:
+                    day = "Friday";
+                    break;
+
+                case 7:
+                    day = "Saturday";
+                    break;
+
+                default:
+                    day = "Invalid Value";
+                    break;
+
+            }
+            Console.WriteLine("Day: " + day);
+
+        }
+    }
+}
